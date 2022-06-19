8aece38311c9f9756f74ed8080d4347ebe25a019
ezaz57 <105972352+ezaz57@users.noreply.github.com>
2022-06-19 13:55:40 +0600

    Create Vector of int.cpp

---
+++ STL/Vector of int.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	#ifndef ONLINE_JUDGE
+  	freopen("input.in", "r", stdin);
+  	freopen("output.out", "w", stdout);
+  	#endif
+ 
+	vector<char> v;
+
+	int n;
+	cin>>n;
+
+	// get User Input in Vector
+	for(int i=0;i<n;i++){
+		int x;
+		cin>>x;
+
+		v.push_back(x);
+	}
+
+	// print Vector
+	for(auto it = v.begin(); it!=v.end();it++){
+		cout<<" "<<*it;
+	}
+
+	return 0;
+}
